daa50713fd45d44262a6c72a1ecb24a11f8636ac
Badru Ndawula <badrundawula24@gmail.com>
2023-11-24 22:22:01 +0300

    rearrange test files

--- src/testfiles/Home.test.js
+++ src/testfiles/Home.test.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Details from './components/Details';
-import store from './redux/store';
+import Home from '../components/Home/Home';
+import store from '../redux/store';
 
-test('renders Details component correctly', () => {
+test('renders Home component correctly', () => {
   const { asFragment } = render(
     <Provider store={store}>
       <Router>
-        <Details />
+        <Home />
       </Router>
     </Provider>,
   );
--- src/testfiles/Search.test.js
+++ src/testfiles/Search.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import Search from './components/Home/Search';
+import Search from '../components/Home/Search';
 
 test('renders Search component correctly', () => {
   const setSearchQueryMock = jest.fn();
